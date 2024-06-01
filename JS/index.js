67cfcc22189993727571e42af661313af2b88daa
Ahmed-Hesham-Emam <ahmedh.emam99@gmail.com>
2024-06-01 20:28:21 +0300

    V1.0.3

--- JS/index.js
+++ JS/index.js
@@ -102,19 +102,13 @@ function login() {
       localStorage.setItem("currentUser", dataBase[i].Name);
       window.location.href = "Welcome.html";
     } else if (
-      !dataBase[i].Email.toLowerCase().includes(loginEmail.value.toLowerCase())
-    ) {
-      EmailCheck.classList.replace("d-none", "d-block");
-      passCheck.classList.replace("d-block", "d-none");
-      logCheck.classList.replace("d-block", "d-none");
-    } else if (
       !dataBase[i].Email.toLowerCase().includes(
         loginEmail.value.toLowerCase()
       ) &&
       !dataBase[i].Password.includes(loginPassword.value)
     ) {
-      passCheck.classList.replace("d-block", "d-none");
       logCheck.classList.replace("d-none", "d-block");
+      passCheck.classList.replace("d-block", "d-none");
       EmailCheck.classList.replace("d-block", "d-none");
     } else if (
       loginEmail.value.toLowerCase() == dataBase[i].Email.toLowerCase() &&
@@ -122,6 +116,12 @@ function login() {
     ) {
       passCheck.classList.replace("d-none", "d-block");
       EmailCheck.classList.replace("d-block", "d-none");
+      logCheck.classList.replace("d-block", "d-none");
+    } else if (
+      !dataBase[i].Email.toLowerCase().includes(loginEmail.value.toLowerCase())
+    ) {
+      EmailCheck.classList.replace("d-none", "d-block");
+      passCheck.classList.replace("d-block", "d-none");
       logCheck.classList.replace("d-block", "d-none");
     } else if (loginEmail.value === "" || loginPassword.value === "") {
       passCheck.classList.replace("d-block", "d-none");
@@ -226,10 +226,9 @@ if (window.location.pathname.endsWith(`/Welcome.html`)) {
 
 if (window.location.pathname.endsWith(`/Welcome.html`)) {
   if (!localStorage.getItem("currentUser")) {
-    caches.delete(`/Welcome.html`);
-    // .then(() => {
-    //   window.location.href = "index.html";
-    // });
+    caches.delete(`/Welcome.html`).then(() => {
+      window.location.href = "index.html";
+    });
   }
 }
 
